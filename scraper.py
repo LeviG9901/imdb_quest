a055ac9d0807a1de0d80fa3e293cf8be81fdc6c0
Gvárdián Levente <gvardian.levente@gmail.com>
2022-03-02 12:16:51 +0100

    Update scraper.py
    Put everything under a function called scrape(), and start to create for cycle

--- scraper.py
+++ scraper.py
@@ -1,14 +1,20 @@
 from imdb import Cinemagoer
 
-# Create an instance
-ia = Cinemagoer()
 
-# Get the top 250 movie from IMDB
-top250 = ia.get_top250_movies()
+# Function for scraping the top 20 movie
+def scrape():
+    # Create an instance
+    ia = Cinemagoer()
 
-# Creating the original top 250 movie list as empty list
-org_250 = []
+    # Get the top 250 movie from IMDB
+    top250 = ia.get_top250_movies()
 
-# Creating a movie dictionary
-# We will need the movie title, rating, votes, and number of how many oscar the movie won
-movie_dict = {'title': [], 'rating': [], 'votes': [], 'oscars': []}
+    # Creating the original top 250 movie list as empty list
+    org_250 = []
+
+    # Creating a movie dictionary
+    # We will need the movie title, rating, votes, and number of how many oscar the movie won
+    movie_dict = {'title': [], 'rating': [], 'votes': [], 'oscars': []}
+
+    # For cycle for getting top20 movie
+    # for index in range(len(top250)):
