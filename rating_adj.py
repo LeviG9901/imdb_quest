a8a678bf9651307d5320456e7bad8287229c704e
Gvárdián Levente <gvardian.levente@gmail.com>
2022-03-02 18:17:59 +0100

    Update rating_adj.py
    Creating the diff and penalty variable, and rounding the diff and ratings, then changing every movie rating with penalty variable.

--- rating_adj.py
+++ rating_adj.py
@@ -15,8 +15,17 @@ def penalizer(mylist):
         # Because the first element is the benchmark
         if index != 0:
             # Calculating the vote number difference between the first and indexed movie
-            diff = mylist[0]['votes'] - mylist[index]['votes']
-            print('Diff: ', diff)
+            # Round the diff number
+            diff = round((mylist[0]['votes'] - mylist[index]['votes'])/1000000)
 
+            # Penalty variable for multiplying the diff with 0.1
+            penalty = round(diff * 0.1, 1)
+
+            # Changing original movie rating based on the gained penalty score
+            mylist[index]['rating'] -= penalty
+            # Rounding up the rating
+            mylist[index]['rating'] = round(mylist[index]['rating'], 1)
+            print(mylist[index]['rating'])
 
     print("End of the penalizer function!")
+    return mylist
