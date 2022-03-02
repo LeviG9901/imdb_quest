91f64b4f48c7efa7ed00ba4b7da5d1888eba63b2
Gvárdián Levente <gvardian.levente@gmail.com>
2022-03-02 18:06:35 +0100

    Update rating_adj.py
    Calculating the difference between the first movie votes with the the indexed one

--- rating_adj.py
+++ rating_adj.py
@@ -6,6 +6,17 @@ def penalizer(mylist):
 
     # Sorting the given list in descending order
     # Lambda function for sorting this list of dictionaries by the key 'votes'
-    mylist.sort(reversed=True, key=lambda i: i['votes'])
+    mylist.sort(reverse=True, key=lambda i: i['votes'])
+
+    # Making for loop to iterate through the list
+    for index in range(len(mylist)):
+
+        # This if needs for missing the first element of the list
+        # Because the first element is the benchmark
+        if index != 0:
+            # Calculating the vote number difference between the first and indexed movie
+            diff = mylist[0]['votes'] - mylist[index]['votes']
+            print('Diff: ', diff)
+
 
     print("End of the penalizer function!")
