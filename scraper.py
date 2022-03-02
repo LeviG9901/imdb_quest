e105b31458741ee3b1f263782f8900eaa30e8427
Gvárdián Levente <gvardian.levente@gmail.com>
2022-03-02 12:12:18 +0100

    Update scraper.py
    Creating an empty list for the original top 250 movie, and also creating an empty dictionary which will be a template for every movie

--- scraper.py
+++ scraper.py
@@ -6,3 +6,9 @@ ia = Cinemagoer()
 # Get the top 250 movie from IMDB
 top250 = ia.get_top250_movies()
 
+# Creating the original top 250 movie list as empty list
+org_250 = []
+
+# Creating a movie dictionary
+# We will need the movie title, rating, votes, and number of how many oscar the movie won
+movie_dict = {'title': [], 'rating': [], 'votes': [], 'oscars': []}
