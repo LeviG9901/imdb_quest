3b51f55a5a2ffdc671807651f8e2b3f6bcfb7093
Gvárdián Levente <gvardian.levente@gmail.com>
2022-03-02 18:00:08 +0100

    Update rating_adj.py
    Sorting the list descending order

--- rating_adj.py
+++ rating_adj.py
@@ -1,3 +1,11 @@
 
 
 def penalizer(mylist):
+
+    print("Beginning of the penalizer function...")
+
+    # Sorting the given list in descending order
+    # Lambda function for sorting this list of dictionaries by the key 'votes'
+    mylist.sort(reversed=True, key=lambda i: i['votes'])
+
+    print("End of the penalizer function!")
