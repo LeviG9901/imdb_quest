3485d16e3b58bc8789833c9099739b6cf4cca53f
Gvárdián Levente <gvardian.levente@gmail.com>
2022-03-03 10:39:13 +0100

    Update unit_test.py
    Creating test for scrape function

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,9 +1,15 @@
 import unittest
+import scraper
+import rating_adj
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestFunctions(unittest.TestCase):
+    # Testing scraper function
+    # Length of the returned list must be 20, and type must be list
+    def test_scraper(self):
+        og_top20 = scraper.scrape()
+        self.assertEqual(len(og_top20), 20)
+        self.assertEqual(type(og_top20), list)
 
 
 if __name__ == '__main__':
