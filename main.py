c87b6cce597efa44f75fc453d5249eaf0410a774
Gvárdián Levente <gvardian.levente@gmail.com>
2022-03-03 09:10:24 +0100

    Update main.py
    Writing out all of the original Top 20 movie from imdb into JSON file

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import scraper
 import rating_adj
 import copy
+import json
 
 print("The program has started...")
 
 # Getting the original top 20 movie
 og_top20 = scraper.scrape()
+
+# Writing original top 20 movie into JSON file
+with open('top20_movie.json', 'w', encoding='utf-8') as f:
+    json.dump(og_top20, f, ensure_ascii=False, indent=4)
 
 # Deep copy the original Top20 movie list with copying every dictionary inside it
 copy_top20 = []
@@ -19,6 +24,7 @@ p_list = rating_adj.penalizer(copy_top20)
 # Modifying movie ratings according to won oscar number
 # PO means Penalized and Oscar calculatored
 po_list = rating_adj.oscar_calculator(p_list)
+
 
 
 print("The program finished running!")
