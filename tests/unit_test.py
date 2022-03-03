fee918895d20529385a60ab1fb484643f8c0c314
Gvárdián Levente <gvardian.levente@gmail.com>
2022-03-03 10:50:19 +0100

    Update unit_test.py
    Creating test for oscar calculator function and modifying penalizer function with checking the length of the MOVIES, and not 2

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -14,7 +14,9 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual(type(og_top20), list)
 
     # Testing penalizer function with two given movies
-    # Length of the list will be 2, and type must be list
+    # Length of the returned list must be equal to the length of the MOVIES
+    # Returned type must be a list
+    # Checking the change in the movie ratings
     def test_penalizer(self):
         # Get a deep copy of the MOVIES list of dictionaries
         copy_l = []
@@ -23,11 +25,27 @@ class TestFunctions(unittest.TestCase):
             copy_l.append(d2)
         # p_list for storing the returned list
         p_list = rating_adj.penalizer(copy_l)
-        self.assertEqual(len(p_list), 2)
+        self.assertEqual(len(p_list), len(MOVIES))
         self.assertEqual(type(p_list), list)
         self.assertEqual(p_list[0]['rating'], 9.2)
         self.assertEqual(p_list[1]['rating'], 8.4)
 
+    # Testing oscar calculator function with two given movies
+    # Length of the returned list must be equal to the length of the MOVIES
+    # Returned type must be a list
+    # Checking the change in the movie ratings
+    def test_oscar_calculator(self):
+        # Get a deep copy of the MOVIES list of dictionaries
+        copy_l = []
+        for li in MOVIES:
+            d2 = copy.deepcopy(li)
+            copy_l.append(d2)
+        # o_list for storing the returned list
+        o_list = rating_adj.oscar_calculator(copy_l)
+        self.assertEqual(len(o_list), len(MOVIES))
+        self.assertEqual(type(o_list), list)
+        self.assertEqual(o_list[0]['rating'], 9.2)
+        self.assertEqual(o_list[1]['rating'], 9.7)
 
 if __name__ == '__main__':
     unittest.main()
